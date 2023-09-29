d3e0312af985e99c20d831348e0a06223a7baf93
02ggang9 <02ggang9@gmail.com>
2023-09-29 20:32:59 +0900

    feat(apply): 스프링 AOp 구현5 - 어드바이스 순서

--- src/main/java/hello/aop/order/aop/AspectV4Pointcut.java
+++ src/main/java/hello/aop/order/aop/AspectV4Pointcut.java
@@ -4,8 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 
 @Slf4j
+@Order(1)
 @Aspect
 public class AspectV4Pointcut {
 
